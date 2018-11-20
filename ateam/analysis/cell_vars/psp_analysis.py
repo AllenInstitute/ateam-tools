4061531975a123deb1fcdea4c2d6c6da57d50a49
Tom Chartrand <tmchartrand@gmail.com>
2018-11-19 16:35:36 -0800

    update cellvars methods for mpi compatibility

--- ateam/analysis/cell_vars/psp_analysis.py
+++ ateam/analysis/cell_vars/psp_analysis.py
@@ -2,6 +2,11 @@ import numpy as np
 import pandas as pd
 import bmtk.analyzer.cell_vars as cell_vars
 
+def data_multicell(cellvars, gid_list, var_name=VAR_UNKNOWN, time_window=None, compartments='origin'):
+        data_iter = (cellvars.data(gid, var_name, time_window, compartments) for gid in gid_list)
+        data_stacked = np.stack(data_iter)
+        return data_stacked
+
 def epsp_analysis(report_file=None, config_file='config.json', t_min=100, t_duration=1000, t_spike=None):
     """ Analyze epsp's as contained in a bionet cell vars report,
     for a simulation consisting of a single presynaptic spike,
@@ -20,21 +25,22 @@ def epsp_analysis(report_file=None, config_file='config.json', t_min=100, t_dura
     gids = np.array(var_report.gids)
 
     t_min = t_spike or t_min
-    v_all = var_report.data(gids, 'v', time_window=[t_min, t_min+t_duration])
+    v_all = data_multicell(cellvars, gids, 'v', time_window=[t_min, t_min+t_duration])
+    # v array has shape (cells, time)
 
     i_bl = 0
     tt = var_report.time_trace
     if t_spike is None:
         nz = np.nonzero(v_all > v_spike)
-        cells_spike = np.unique(nz[1])
+        cells_spike = np.unique(nz[0])
         assert(len(cells_spike)==1)
         i_bl = nz[0][0]
         t_spike = tt[i_bl]
-        v_all = v_all[i_bl:,:]
+        v_all = v_all[:,i_bl:]
     
 
-    v_peak = np.max(v_all, axis=0)
-    v_bl = v_all[0,:]
+    v_peak = np.max(v_all, axis=-1)
+    v_bl = v_all[:,0]
     amp = v_peak - v_bl
     cells_epsp = np.flatnonzero((amp_min < amp) & (amp < amp_max))
 
@@ -43,7 +49,7 @@ def epsp_analysis(report_file=None, config_file='config.json', t_min=100, t_dura
     i_start = np.zeros(n_conn, dtype=int)
     i_end = np.zeros(n_conn, dtype=int)
     for i, cell in enumerate(cells_epsp):
-        i_up = np.flatnonzero(v_all[:,cell] > v_mid[cell])
+        i_up = np.flatnonzero(v_all[cell,:] > v_mid[cell])
         i_start[i] = i_up[0]
         i_end[i] = i_up[-1]
 

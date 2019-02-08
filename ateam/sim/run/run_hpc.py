47e5700e9f23be42aa5467f65297a2fb332bee56
Tom Chartrand <tmchartrand@gmail.com>
2019-02-08 15:17:36 -0800

    singlecell batch and multiple sim tools

--- ateam/sim/run/run_hpc.py
+++ ateam/sim/run/run_hpc.py
@@ -8,16 +8,19 @@ from ateam.sim.run import runner
 import os.path
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
 parser.add_argument("config")
-parser.add_argument("--conda", default="bmtk_ateam")
-parser.add_argument("--mem", "-m", default="4g")
-parser.add_argument("--queue","-q", default="celltypes")
-parser.add_argument("--time","-t", default="00:05:00")
+parser.add_argument("--conda", default="bmtk_ateam", help="Conda environment name")
+parser.add_argument("--time","-t", default="00:10:00")
 parser.add_argument("--nodes", "-n", default=1, type=int)
+parser.add_argument("--mem", "-m", default="4g", help="Memory usage (only used for single node jobs?)")
+parser.add_argument("--procs","-p", type=int, help="Number of processors. Overrides nodes and ppn.")
+parser.add_argument("--queue","-q", default="celltypes")
 parser.add_argument("--ppn", type=int)
-parser.add_argument("--procs","-p", type=int)
-parser.add_argument("--norerun", action='store_true')
+parser.add_argument("--norerun", action='store_true', help="Prevent overwriting existing sim output")
+parser.add_argument("--export_vars", action='store_true')
 
 def main(args_list=None):
     args = parser.parse_args(args_list)

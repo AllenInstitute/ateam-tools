8655f80db5c8ed8edb9f8e4e7ff31e4067ae1397
Tom Chartrand <tmchartrand@gmail.com>
2019-03-21 14:20:32 -0700

    multicompartment plotting

--- ateam/analysis/cell_vars/cell_vars.py
+++ ateam/analysis/cell_vars/cell_vars.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def plot_v(config_file, ax=None, colors=None, gids=None, stack_sep=40, show=False):
     plot_var('v', config_file, ax=None, colors=None, gids=None, stack_sep=stack_sep, show=False)
 
-def plot_var(var_name, config_file, ax=None, colors=None, gids=None, stack_sep=None, show=False):
+def plot_var(var_name, config_file, ax=None, colors=None, gids=None, stack_sep=None, compartments='origin', show=False):
     ax = ax or plt.gca()
     report_file = cell_vars.get_cell_report(config_file)
     var_report = cell_vars.CellVarsFile(report_file)
@@ -15,9 +15,9 @@ def plot_var(var_name, config_file, ax=None, colors=None, gids=None, stack_sep=N
     for i, gid in enumerate(gids):
         color = colors[i] if colors else 'b'
         if stack_sep:
-            ax.plot(tt, var_report.data(gid, var_name) + i*stack_sep, color=color)
+            ax.plot(tt, var_report.data(gid, var_name, compartments=compartments) + i*stack_sep, color=color)
         else:
-            ax.plot(tt, var_report.data(gid, var_name), color=color)
+            ax.plot(tt, var_report.data(gid, var_name, compartments=compartments), color=color)
     if show:
         plt.show()
                 

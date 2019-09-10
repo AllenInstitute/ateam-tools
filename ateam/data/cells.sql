167b94cf03fd7eddaae5eaabc18e5cd2b579e6a7
Tom Chartrand <tom.chartrand@alleninstitute.org>
2019-09-10 16:51:24 -0700

    expand lims queries

--- ateam/data/cells.sql
+++ ateam/data/cells.sql
@@ -5,7 +5,7 @@ with dendrite_type as
     join specimen_tags st on sts.specimen_tag_id = st.id
     where st.name like 'dendrite type%%'
     )
-SELECT sp.id, sp.name,
+SELECT sp.id, sp.name, sp.created_at,
 csl.normalized_depth, csl.soma_depth_um,
 struct.name AS structure, 
 donors.name AS donor_name, 
@@ -21,6 +21,15 @@ LEFT JOIN structures struct ON sp.structure_id = struct.id
 LEFT JOIN structures layer ON sp.cortex_layer_id = layer.id
 LEFT JOIN cell_soma_locations csl ON csl.specimen_id = sp.id
 LEFT JOIN dendrite_type dt ON sp.id = dt.specimen_id
+LEFT JOIN neuronal_models nm ON sp.id = nm.specimen_id
+LEFT JOIN
+    (
+        SELECT nr.specimen_id, f.filename FROM 
+        neuron_reconstructions nr JOIN well_known_files f ON nr.id = f.attachable_id 
+        WHERE f.well_known_file_type_id = 303941301
+        AND NOT nr.superseded
+    ) AS nr
+ON sp.id = nr.specimen_id
 JOIN well_known_files nwb ON nwb.attachable_id = err.id
 JOIN well_known_file_types ftype ON nwb.well_known_file_type_id = ftype.id
 WHERE nwb.attachable_type = 'EphysRoiResult'
--- ateam/data/lims.py
+++ ateam/data/lims.py
@@ -15,7 +15,8 @@ class LimsReader(object):
         super(LimsReader, self).__init__()
         self.engine = sql.create_engine(base_uri)
 
-    def get_cells_df(self, project_id=None, project_code=None, cells_list=None):
+    def get_cells_df(self, project_id=None, project_code=None, cells_list=None, has_peri_model=False,
+                    has_reconstruction=False):
         """Get info for cell specimens in LIMS with ephys data
         Select by project (code or id) or list of cell specimen IDs
         
@@ -35,6 +36,11 @@ class LimsReader(object):
             sql += " AND projects.code = '{}'".format(project_code)
         if cells_list is not None:
             sql += " AND sp.id IN ({})".format(", ".join([str(cell) for cell in cells_list]))
+        if has_peri_model:
+            template = model_template_dict['peri']
+            sql += " AND nm.neuronal_model_template_id = {}".format(template)
+        if has_reconstruction:
+            sql += " AND nr.filename IS NOT NULL"
         cells_df = pd.read_sql(sql, self.engine, index_col='id')
         return cells_df
 
@@ -42,23 +48,31 @@ class LimsReader(object):
         sql = "SELECT name from ephys_stimulus_types"
         return self.list_query(sql)
 
-    def get_sweeps(self, cell_id, sweep_type, passed_only=False):
+    def get_sweeps(self, cell_id, sweep_type=None, description=None, passed_only=False, spiking=None, depolarizing=None):
         """Get a list of sweeps for a single cell specimen, by sweep type name
         """
         sql = """SELECT sw.sweep_number
             FROM ephys_sweeps sw
             JOIN ephys_stimuli stim ON stim.id = sw.ephys_stimulus_id
             JOIN ephys_stimulus_types stype ON stype.id = stim.ephys_stimulus_type_id
-            WHERE sw.specimen_id = %s AND stype.name LIKE '%s'
-            """ % (cell_id, sweep_type)
+            WHERE sw.specimen_id = {}
+            """.format(cell_id)
+        if sweep_type:
+            sql += " AND stype.name LIKE '%%{}%%'".format(sweep_type)
+        if description:
+            sql += " AND stim.description LIKE '%%{}%%'".format(description)
         if passed_only:
             sql += " AND sw.workflow_state LIKE '%%passed'"
+        if spiking is not None:
+            sql += " AND sw.num_spikes {}".format("> 0" if spiking else "= 0")
+        if depolarizing is not None:
+            sql += " AND sw.stimulus_amplitude {}".format("> 0" if depolarizing else "< 0")
         return self.list_query(sql)
 
     def get_sweep_info(self, cell_id, sweep_type=None):
         """Get a table of information for all sweeps of a given cell specimen
         """
-        sql = """SELECT sw.sweep_number, sw.workflow_state,
+        sql = """SELECT sw.*, 
             stype.name, stim.description
             FROM ephys_sweeps sw
             JOIN ephys_stimuli stim ON stim.id = sw.ephys_stimulus_id
@@ -67,7 +81,7 @@ class LimsReader(object):
             """ % (cell_id)
         if sweep_type:
             sql += " AND stype.name LIKE '{}'".format(sweep_type)
-        return pd.read_sql(sql, self.engine, index_col='sweep_number')
+        return pd.read_sql(sql, self.engine)
   
     def get_nwb_path_from_lims(self, cell_id, get_sdk_version=False):
         sql = """
@@ -89,7 +103,7 @@ class LimsReader(object):
         sql = """
             SELECT f.storage_directory || f.filename AS nwb_path FROM 
             neuron_reconstructions n JOIN well_known_files f ON n.id = f.attachable_id 
-            AND n.specimen_id = {id} AND f.well_known_file_type_id = 303941301
+            WHERE n.specimen_id = {id} AND f.well_known_file_type_id = 303941301
             AND NOT n.superseded
             """
         # Not sure whether this manual flag is needed, found it in Nathan's code...
@@ -98,16 +112,7 @@ class LimsReader(object):
         return self.single_result_query(sql.format(id=cell_id))
     
     def get_sim_nwb_path_from_lims(self, cell_id, model_type='peri', passed_only=True):
-        template_dict = {
-        'allactive': 491455321,
-        'lif4': 471355161,
-        'lif5': 395310498,
-        'lif2': 395310479,
-        'lif3': 395310475,
-        'lif1': 395310469,
-        'peri': 329230710
-        }
-        template = template_dict[model_type]
+        template = model_template_dict[model_type]
         sql = """
             SELECT nwb.storage_directory || nwb.filename AS nwb_path
             FROM neuronal_models nm
@@ -120,6 +125,18 @@ class LimsReader(object):
             """
         if passed_only:
             sql += "AND runs.workflow_state = 'passed'"
+        return self.single_result_query(sql.format(id=cell_id, template=template))
+
+    def get_model_path_from_lims(self, cell_id, model_type='peri'):
+        template = model_template_dict[model_type]
+        sql = """
+            SELECT file.storage_directory || file.filename AS nwb_path
+            FROM neuronal_models nm
+            JOIN well_known_files file ON file.attachable_id = nm.id
+            JOIN well_known_file_types ftype ON file.well_known_file_type_id = ftype.id
+            WHERE nm.specimen_id = {id}
+            AND nm.neuronal_model_template_id = {template}
+            """
         return self.single_result_query(sql.format(id=cell_id, template=template))
 
     def single_result_query(self, sql):
@@ -137,6 +154,18 @@ class LimsReader(object):
         results = [s[0] for s in test.fetchall()]
         return results
 
+def node_dict_from_lims(cell_id, model_type='peri'):
+    """Create a dict of node properties for BMTK simulations of a model in LIMS.
+    By default use full paths to files to open them in place over the network
+    """
+    lr = LimsReader()
+    swc_path = lr.get_swc_path_from_lims(cell_id)
+    params_path = lr.get_model_path_from_lims(cell_id, model_type=model_type)
+    biophys_props = {
+        'morphology': swc_path,
+        'dynamics_params': params_path,
+    }
+    return biophys_props
 
 from allensdk.core.nwb_data_set import NwbDataSet
 import matplotlib.pyplot as plt
@@ -177,3 +206,13 @@ def get_sweep_v_i_t_lims(cell_id, sweep_num):
     dataset = NwbDataSet(nwb_path)
     v, i, t = get_sweep_v_i_t_from_set(dataset, sweep_num)
     return v, i, t
+
+model_template_dict = {
+        'active': 491455321,
+        'lif4': 471355161,
+        'lif5': 395310498,
+        'lif2': 395310479,
+        'lif3': 395310475,
+        'lif1': 395310469,
+        'peri': 329230710
+        }

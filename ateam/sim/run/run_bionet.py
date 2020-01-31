d016c623d3fd2309ca9951be9ea4ec0c86d6ef7b
Tom Chartrand <tom.chartrand@alleninstitute.org>
2020-01-31 15:38:55 -0800

    run bionet options

--- ateam/sim/run/run_bionet.py
+++ ateam/sim/run/run_bionet.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 
-"""Simulates an example network of 14 cell receiving two kinds of exernal input as defined in configuration file"""
-
 import os, sys
 from bmtk.simulator import bionet
 # Register our modified axon processing with bionet (call after import bionet)
 from ateam.sim import cell_functions 
 
-def create_sim(config_file):
+def create_sim(config_file, setup_output=True):
     conf = bionet.Config.from_json(config_file, validate=True)
-    conf.build_env()
+    conf.build_env(setup_output=setup_output)
 
     graph = bionet.BioNetwork.from_config(conf)
     sim = bionet.BioSimulator.from_config(conf, network=graph)
@@ -22,7 +20,8 @@ def run(config_file):
     graph = bionet.BioNetwork.from_config(conf)
     sim = bionet.BioSimulator.from_config(conf, network=graph)
     sim.run()
-    bionet.nrn.quit_execution()
+    # bionet.nrn.quit_execution()
+    bionet.nrn.reset()
 
 def main():
     if __file__ != sys.argv[-1]:
